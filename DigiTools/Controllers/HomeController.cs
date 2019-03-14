958c4a55a74eddf412152947a5902ea6682e24c5
Jeison Perlaza <gilbertjeison@gmail.com>
2019-03-14 17:05:24 -0500

    p

--- DigiTools/Controllers/HomeController.cs
+++ DigiTools/Controllers/HomeController.cs
@@ -46,36 +46,29 @@ namespace DigiTools.Controllers
         {
             aspNetUsers = daoUser.GetUser(User.Identity.GetUserId());
 
-            
+            var req = Request;
             if (aspNetUsers.IdRol.Equals("65b01f2a-0b46-4d0c-a227-304dc22e2f9d"))
             {
                 var kpiWiewModel = new KpiViewModel();
 
                 //LISTA DE PLANTAS
                 var list = await daoPlantas.GetPlants();
-                list.Insert(0, new Database.plantas() { Id = 0, nombre = "Seleccione planta..." });
+                list.Insert(0, new plantas() { Id = 0, nombre = "Seleccione planta..." });
                 kpiWiewModel.PlantaList = new SelectList(list, "Id", "nombre");
 
                 //LISTA DE TIPOS DE LÍNEA
                 var listTL = await daoTD.GetTypesAsync(LINES);
-                listTL.Insert(0, new Database.tipos_data() { Id = 0, descripcion = "Seleccione tipo de línea..." });
+                listTL.Insert(0, new tipos_data() { Id = 0, descripcion = "Seleccione tipo de línea..." });
                 kpiWiewModel.TipoLineaList = new SelectList(listTL, "Id", "descripcion");
 
                 //LISTA DE TIPOS DE AVERÍAS
                 var listA = await daoTD.GetTypesAsync(BREAKDOWNSTYPES);
-                listA.Insert(0, new Database.tipos_data() { Id = 0, descripcion = "Seleccione tipo de avería..." });
+                listA.Insert(0, new tipos_data() { Id = 0, descripcion = "Seleccione tipo de avería..." });
                 kpiWiewModel.TipoAveriaList = new SelectList(listA, "Id", "descripcion");
 
                 kpiWiewModel.IdDiligenciado = aspNetUsers.Nombres +" " + aspNetUsers.Apellidos;
                 Consecutivo = await daoEwo.GetLastConsecutive();
-                ViewBag.Cons = "00" +Consecutivo;
-
-                //RUTAS DEL SERVIDOR
-                //Debug.WriteLine(Server.MapPath("."));
-                //Debug.WriteLine(Server.MapPath("~"));
-                //Debug.WriteLine(Server.MapPath("/"));
-
-                
+                ViewBag.Cons = "00" + Consecutivo;                
 
                 return View(kpiWiewModel);
             }
